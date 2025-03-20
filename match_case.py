ab301d370444fd5d51ff4fbde14c151ef91678d2
kimdongyoon488 <rlaehddbs54321@gmail.com>
2025-03-20 22:43:03 +0900

    match case문

---
+++ match_case.py
@@ -0,0 +1,30 @@
+season = "봄"
+month = 2
+
+match month:
+    case 1:
+        season = "겨울"
+    case 2:
+        season = "겨울"
+    case 3:
+        season = "봄"
+    case 4:
+        season = "봄"
+    case 5:
+        season = "봄"
+    case 6:
+        season = "여름"
+    case 7:
+        season = "여름"                     
+    case 8:
+        season = "여름"
+    case 9:
+        season = "가을"
+    case 10:
+        season = "가을"
+    case 11:
+        season = "가을"
+    case 12:
+        season = "겨울"
+
+print(f"현재 계절은 {season}입니다")
