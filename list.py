fc679eabb035917f2f26f573d6bbe3f922812902
kimdongyoon488 <rlaehddbs54321@gmail.com>
2025-03-31 18:02:16 +0900

    list를 사용하기 위한 함수들

---
+++ list.py
@@ -0,0 +1,11 @@
+a = [1, 2, 3] # 리스트 초기화
+print(a)
+
+a.append(4) # 값 추가 : append(value)
+print(a)
+
+a.insert(0, 5) # 0번째 index에 5추가
+print(a)
+
+a.remove(5) #리스트에 특정 요소값 삭제 : remove(values)
+print(a)
