2b35602d8739e47ce10007acd6b985760307b48c
kimdongyoon488 <rlaehddbs54321@gmail.com>
2025-04-01 19:29:58 +0900

    list를 사용하기 위한 함수들

--- list.py
+++ list.py
@@ -9,3 +9,13 @@ print(a)
 
 a.remove(5) #리스트에 특정 요소값 삭제 : remove(values)
 print(a)
+
+del a[1] # index가 1번째인 요소값을 삭제
+print(a)
+
+a[0] = 10 # 0번째 index를 10으로 수정
+print(a)
+
+print(len(a)) # 리스트의 길이 알려주는 함수 : len(var)
+
+print(a.index(10)) # 해당 요소값의 위치 알려주는 함수 : index(var)
